a566880f6ef8850382dcc983ec4813cd05f010c1
luigibardellagerbi <luigibardellagerbi@mail.com>
2024-10-02 22:43:28 +0700

    global css + fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,9 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import { Anybody, Raleway } from 'next/font/google'
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const anybody = Anybody({ subsets: ['latin'], weight: ["700","400"] })
+const raleway = Raleway({ subsets: ['latin'], weight: ["700"], variable: "--raleway"})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,8 +17,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+      <body className={`${anybody.className} ${raleway.variable}`}>
+      {children}
       </body>
     </html>
   );
