a40c2cc383e743882d96f012c85c206eab87b030
luigibardellagerbi <luigibardellagerbi@mail.com>
2024-10-04 10:07:48 +0700

    SponsoredTag

---
+++ src/app/components/SponsoredTag.tsx
@@ -0,0 +1,11 @@
+import Tag from "./Tag/Tag";
+import SponsoredIcon from "../public/icons/sponsored.svg"
+
+function SponsoredTag() {
+  return (
+    <Tag size="small" icon={SponsoredIcon} shadow={false}>Sponsored</Tag>
+  )
+}
+
+export default SponsoredTag
+
