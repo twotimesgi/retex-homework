9e3963b5c67b7e3e4903666a1160fd79cdd21c23
luigibardellagerbi <luigibardellagerbi@mail.com>
2024-10-06 10:21:14 +0700

    Cta

--- src/app/components/Cta/Cta.tsx
+++ src/app/components/Cta/Cta.tsx
@@ -3,40 +3,45 @@ import styles from './Cta.module.css'
 import classNames from 'classnames';
 import Image from 'next/image';
 import Link from 'next/link';
-import ArrowRight from '../../public/icons/arrowRight.svg'
+import ArrowRight from '../../public/ui/arrowRight.svg'
 
+type RGB = `rgb(${number}, ${number}, ${number})`;
+type RGBA = `rgba(${number}, ${number}, ${number}, ${number})`;
+type HEX = `#${string}`;
+type VAR = `var(--${string})`
 
 
 interface CtaProps{
     text?: string,
     href?: string,
+    textColor?: "white" | "black" 
     external?: boolean
     onClick?: () => void;
     className?: string,
 }
 
 
-function Cta({text, href, external=false, className, onClick} : CtaProps) {
+function Cta({text, href, external=false, className, onClick, textColor="white"} : CtaProps) {
     const inner = <>
         {text && <span className={styles.ctaText}>{text}</span>}
         <Image src={ArrowRight} className={styles.ctaArrow} alt=""/>
     </>
   if(onClick) return (
-    <button onClick={onClick} className={classNames(
+    <button style={{color: "#000", filter: textColor == "white" ? "invert(1)" : "none" }} onClick={onClick} className={classNames(
         styles.cta, className)}>
     {inner}
     </button>
   )
 
   if(external && href) return (
-    <a href={href} className={classNames(
+    <a style={{color: "#000", filter: textColor == "white" ? "invert(1)" : "none" }} href={href} className={classNames(
         styles.cta, className)}>
     {inner}
     </a>
   )
 
   if (href) return (
-    <Link href={href} className={classNames(
+    <Link style={{color: "#000", filter: textColor == "white" ? "invert(1)" : "none" }} href={href} className={classNames(
         styles.cta, className)}>
     {inner}
     </Link>
